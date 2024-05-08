667e03bb691dd58c20b928dab949789e5f4d6167
GuilhermeMaciel75 <guilhermemacielmelo75@hotmail.com>
2024-05-08 19:53:22 -0300

    fix: python3 conversion

--- src/mnist_loader.py
+++ src/mnist_loader.py
@@ -10,8 +10,9 @@ function usually called by our neural network code.
 
 #### Libraries
 # Standard library
-import cPickle
+import pickle
 import gzip
+
 
 # Third-party libraries
 import numpy as np
@@ -40,7 +41,7 @@ def load_data():
     below.
     """
     f = gzip.open('../data/mnist.pkl.gz', 'rb')
-    training_data, validation_data, test_data = cPickle.load(f)
+    training_data, validation_data, test_data = pickle.load(f, encoding='latin1')
     f.close()
     return (training_data, validation_data, test_data)
 
